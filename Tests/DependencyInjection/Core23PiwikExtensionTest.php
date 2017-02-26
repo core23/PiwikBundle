2016fac4ee4cdb3c49c4003c97c3edbc3c458185
core23 <mail@core23.de>
2017-02-26 11:19:46 +0100

    Added unit tests for dependency injection

---
+++ Tests/DependencyInjection/Core23PiwikExtensionTest.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * (c) Christian Gripp <mail@core23.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Core23\PiwikBundle\Tests\DependencyInjection;
+
+use Core23\PiwikBundle\DependencyInjection\Core23PiwikExtension;
+use Matthias\SymfonyDependencyInjectionTest\PhpUnit\AbstractExtensionTestCase;
+
+class Core23PiwikExtensionTest extends AbstractExtensionTestCase
+{
+    public function testLoadDefault()
+    {
+        $this->setParameter('kernel.bundles', array());
+        $this->load();
+
+        $this->assertContainerBuilderHasAlias('core23.piwik.http.client', 'httplug.client.default');
+        $this->assertContainerBuilderHasAlias('core23.piwik.http.message_factory', 'httplug.message_factory.default');
+    }
+
+    protected function getContainerExtensions()
+    {
+        return array(
+            new Core23PiwikExtension(),
+        );
+    }
+}
