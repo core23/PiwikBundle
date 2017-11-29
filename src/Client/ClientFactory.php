f5d4d9cc92afd2529631560aceb4c34af6d1cfdc
core23 <mail@core23.de>
2017-11-29 22:18:58 +0100

    Fixed PHPStan issues

--- src/Client/ClientFactory.php
+++ src/Client/ClientFactory.php
@@ -31,7 +31,7 @@ final class ClientFactory implements ClientFactoryInterface
      * @param HttpClient     $client
      * @param MessageFactory $messageFactory
      */
-    public function __construct(HttpClient $client = null, MessageFactory $messageFactory)
+    public function __construct(HttpClient $client, MessageFactory $messageFactory)
     {
         $this->client         = $client;
         $this->messageFactory = $messageFactory;
